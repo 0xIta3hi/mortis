dfbca028e8fcdcae7cd3ced6daa1cc6d4074aa95
computerphobe <aryanbharadiyak2006@gmail.com>
2025-12-20 19:42:44 +0530

    basic piplining

--- checks/sqli.py
+++ checks/sqli.py
@@ -1,8 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
 
+# global variables = [white list of all the injectable fields]
+# response parameters = {res_time, content_length, }
+# base_line_parameters = {base_line_time, base_line_length, etc}
+# dummy_data = {username, password, email, age, a static dictionary with generic form fields data}
+
+
 def get_forms():
-    # return the input fields
+    # get the forms from the html page by first fetching it.
+    # Only fields in the white list.
+    # add them to input_fields dictionary which includes injectable fields from whitelist. 
+    # return the input_fields dictionary.
+    # For required fields -> required ? True : False.
+    pass
+
+
+# baseline response -> inject fields one at a time and compare the response with baseline response -> difference ? yes : no 
+# function for the baseline dictionary generation
+def base_response():
+    # ṃake a initial request by getting all the injectable fields with actual dummy data from the dictionary 
+    # Exract the injectable fields from this response along with there values and add themm to the baseline_dictionary.
     pass
 
 
@@ -11,4 +29,5 @@ def sql_injection():
     # one by one inject different payloads in the input fields.
     # return the responses per payload..
     pass
+
 
