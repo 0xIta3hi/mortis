c1ac4ad10eaf1cb0db265adc36a85efc59ddaa4f
computerphobe <aryanbharadiyak2006@gmail.com>
2026-01-03 20:04:24 +0530

    corrected the data structure in the parse_form function

--- checks/sqli.py
+++ checks/sqli.py
@@ -11,7 +11,6 @@ def get_forms():
     url = input("Enter the url: ")
     response = requests.get(url)
     form_text = response.text 
-    print(form_text)
     return form_text
 
 
@@ -19,30 +18,33 @@ def parse_forms(text):
     """ Text refers to the text returned from the get_forms() function. """
     soup = BeautifulSoup(text, "html.parser")
     forms = soup.find_all("form")
-    all_forms_data = [] 
+    all_form_data = []
     for form in forms:
-        input_fields = form.find_all(['input', 'textarea', 'select'])
-        form_data = []
-        for field in input_fields:
-            input_data = {
-                "name": field.get('name'),
-                "type":field.get("type"),
-                "required":field.has_attr('required')
-            }
-            form_data.append(input_data)
         action = form.get('action')
         method = form.get('method')
-        print(f'form {input} has action:{action} and method: {method}')
-        form_data.append(action)
-        form_data.append(method)
-        all_forms_data.append(form_data)
-    return all_forms_data
+        form_field = []
+        input_fields = form.find_all(['input', 'textarea', 'submit'])
+        for field in input_fields:
+            input_data = {
+                "name":field.get('name'),
+                "required": field.has_attr('required'),
+                "type": field.get('type'),
+            }
+            form_field.append(input_data)
+        form_object = {
+            "action": action,
+            "method": method,
+            "form_data": form_field
+        }
+        all_form_data.append(form_object)
+    return all_form_data
 
 
 
 # baseline response -> inject fields one at a time and compare the response with baseline response -> difference ? yes : no 
 # function for the baseline dictionary generation
 def base_response():
+
     pass
 
 
@@ -52,5 +54,6 @@ def sql_injection():
     # return the responses per payload..
     pass
 
-
-get_forms()
+form_text = get_forms()
+form_data = parse_forms(form_text)
+print(form_data)
